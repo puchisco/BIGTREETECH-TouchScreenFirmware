97695c19538961dcbfdb84d1d8d9de575c59e308
Ivan Puccinelli <ivan.puccinelli@gmail.com>
2021-03-03 16:23:23 -0300

    .

--- TFT/src/User/Configuration.h
+++ TFT/src/User/Configuration.h
@@ -48,7 +48,7 @@
  *          SPANISH,    FRENCH,   PORTUGUESE,  ITALIAN,    POLISH,    SLOVAK,        DUTCH,
  *          HUNGARIAN,  TURKISH,  GREEK,       SLOVENIAN,  CATALAN,   TRAD_CHINESE,  UKRAINIAN
  */
-#define DEFAULT_LANGUAGE ENGLISH  // Default: ENGLISH
+#define DEFAULT_LANGUAGE SPANISH  // Default: ENGLISH
 
 /**
  * Default Touch Mode Color Options
